b7a24d4e62ca4cf522643269396e2073e71a3957
Taby <forwardpivot@proton.me>
2022-06-30 11:09:24 -0500

    Have a function that logs which key was pressed

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ const Player =  () => {
     const player = document.createElement('div');
     player.classList.add('player');
     document.body.appendChild(player);
+
 }
 
+const move = (e) => {
+    switch(e.code) {
+        case 'KeyW':
+            console.log("W was pressed");
+            break;
+        case 'KeyA':
+            console.log("A was pressed");
+            break;
+        case 'KeyS':
+            console.log("S was pressed");
+            break;
+        case 'KeyD':
+            console.log("D was pressed");
+            break;
+    }
+}
 
     const player = Player();
 
+    window.addEventListener('keydown', move);
