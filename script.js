0cbca2a74b10daec21f746d52a7555170d9d1c96
Taby <forwardpivot@proton.me>
2022-06-30 10:35:52 -0500

    Created an object that creates the player

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Grabbing the player
-const player = document.querySelector(".player");
+// I have to create an object first that will be the player. SO that way it can use methods, and stuff
+
+const Player =  () => {
+    const player = document.createElement('div');
+    player.classList.add('player');
+    document.body.appendChild(player);
+}
+
+
+    const player = Player();
+
